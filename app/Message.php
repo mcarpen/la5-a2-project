68cb129118656bc4b1c7116729c6a0436fbebcc1
Matthieu Carpentier <matthieu.carpentier75@gmail.com>
2018-01-07 15:56:42 +0100

    add conversation and update message table

--- app/Message.php
+++ app/Message.php
@@ -10,4 +10,9 @@ class Message extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
 }
